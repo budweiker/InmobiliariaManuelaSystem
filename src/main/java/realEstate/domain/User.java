6ae6da84d792c977ad99495d4f5467922bedf155
santiago <santiagorodriguezf@rafaeljmejia.edu.co>
2026-05-02 17:11:11 -0500

    Creacion de clase padre user, y getters y setters para todas las clases

--- src/main/java/realEstate/domain/User.java
+++ src/main/java/realEstate/domain/User.java
@@ -1,14 +1,14 @@
 package realEstate.domain;
 
 public class User {
-    protected int id; //Como es una clase padre, ps si ponemos private las otras clases no lo van a poder ver y si es public no estamos usando encapsulacion, entonces la mejor opcion es protected
+    protected int id;  //Como es una clase padre, ps si ponemos private las otras clases no lo van a poder ver y si es public no estamos usando encapsulacion, entonces la mejor opcion es protected
     protected String nombre;
     protected float balance;
     protected String telefono;
     protected String correo;
     
     public User(){
-        
+
     }
  public int getId() {
         return id;
