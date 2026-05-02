442cc30d1fa765e3364408a2e4ff3dcc0ff83b00
santiago <santiagorodriguezf@rafaeljmejia.edu.co>
2026-05-02 16:38:32 -0500

    optimizacion codigo menu app

--- src/main/java/realEstate/userInterface/MenuApp.java
+++ src/main/java/realEstate/userInterface/MenuApp.java
@@ -2,15 +2,14 @@ package realEstate.userInterface;
 import realEstate.util.TypeValidator;
 public class MenuApp {
     TypeValidator tv = new TypeValidator();
-    int option;
+    int option = 0;
     public void mainMenu(){
-        boolean running = true; // esta variable para que sirve? 😭😭😭😭😭
-        while (running){
         String a = "Bienvenido a inmobiliaria Manuela Bermudez Realtor\n" +
                             "Selecciona una opción:\n" +
                             "1. Explorar propiedades\n" +
                             "2. Iniciar sesión\n" +
                             "3. Salir";
+        while (option != 3){
         option = tv.leerIntEnRango(1, 3, a);
             switch (option) {
                 case 1:
@@ -21,7 +20,6 @@ public class MenuApp {
                     break;
                 case 3:
                     tv.Mensaje("Has salido del sistema gracias por visitas mucha suerte =D! ");
-                    return;
             }
         }
     }
