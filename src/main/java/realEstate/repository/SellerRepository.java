d77ea5a9440f85910be78220c3605bca3deb61b1
santiago <santiagorodriguezf@rafaeljmejia.edu.co>
2026-05-03 14:42:04 -0500

    repository vendedores

--- src/main/java/realEstate/repository/SellerRepository.java
+++ src/main/java/realEstate/repository/SellerRepository.java
@@ -1,6 +1,19 @@
 package realEstate.repository;
+import java.util.Collection;
 import java.util.HashMap;
-
+import realEstate.domain.Seller;
 public class SellerRepository {
-    
+    HashMap<String, Seller> vendedores = new HashMap<>();
+    public void agregar(Seller s){
+        vendedores.put(s.getNombre(), s);
+    }
+    public void eliminar(String nombre){
+        vendedores.remove(nombre);
+    }
+    public Seller buscar(String nombre){
+        return vendedores.get(nombre);
+    }
+    public Collection<Seller> listarTodos(){
+        return vendedores.values();
+    }
 }
