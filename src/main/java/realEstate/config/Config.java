afc426061ef78d150a0214404cd5581187e11fed
santiago <santiagorodriguezf@rafaeljmejia.edu.co>
2026-05-03 14:53:41 -0500

    inicio del config

--- src/main/java/realEstate/config/Config.java
+++ src/main/java/realEstate/config/Config.java
@@ -1,5 +1,22 @@
 package realEstate.config;
 
+import realEstate.repository.BuyerRepository;
+import realEstate.repository.PropertyRepository;
+import realEstate.repository.SellerRepository;
+import realEstate.service.BuyerService;
+import realEstate.service.PropertyService;
+import realEstate.service.SellerService;
+
 public class Config {
     
+    // Repositories
+    private PropertyRepository propertyRepo = new PropertyRepository();
+    private SellerRepository sellerRepo = new SellerRepository();
+    private BuyerRepository buyerRepo = new BuyerRepository();
+
+    // Services 
+    private PropertyService propertyService = new PropertyService(propertyRepo);
+    private SellerService sellerService = new SellerService(sellerRepo, propertyService);
+    private BuyerService buyerService = new BuyerService(buyerRepo, propertyService);
+
 }
