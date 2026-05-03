a4da2a3f3153a204814a411ee736d0e936f856de
santiago <santiagorodriguezf@rafaeljmejia.edu.co>
2026-05-03 14:38:51 -0500

    eliminacion de un objeto innesecario en propertyRepository(TypeValidator)

--- src/main/java/realEstate/repository/PropertyRepository.java
+++ src/main/java/realEstate/repository/PropertyRepository.java
@@ -2,36 +2,20 @@ package realEstate.repository;
 import java.util.Collection;
 import java.util.HashMap;
 import realEstate.domain.Property;
-import realEstate.util.TypeValidator;
-
 public class PropertyRepository {
     HashMap<String, Property> propiedades = new HashMap<>();
-
-
-
-
-    static TypeValidator tv = new TypeValidator();
-    
     public void agregar(Property p){
         propiedades.put(p.getnombrePropiedad(), p);
-
-
     }
     public void eliminar(String nombre){
         propiedades.remove(nombre);
-        
-
     }
     public Property buscar(String nombre){
         return propiedades.get(nombre);
-
     }
     public Collection<Property> listarTodos(){
         return propiedades.values();
-
     }
-
-    
 }
 
     
