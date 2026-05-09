1f47c86b7b459607c64bbd5917193832d46a1c70
matiasramirez25072010@gmail.com <trabajo4>
2026-05-08 19:55:33 -0500

    Completacion de todos los servicies

--- src/main/java/realEstate/service/SellerService.java
+++ src/main/java/realEstate/service/SellerService.java
@@ -1,13 +1,33 @@
 package realEstate.service;
 import realEstate.domain.Seller;
+import realEstate.persistence.repository.SellerRepository;
 import realEstate.domain.Property;
+import realEstate.util.TypeValidator;
+import java.util.Collection;
 
 public class SellerService {
-    public Seller registrarVendedor(){
+    static TypeValidator tv = new TypeValidator();
+    private SellerRepository repository;
+    public SellerService(){
+        this.repository = new SellerRepository();
+    }
+    public Collection<Seller> SelectAllSellers(){
+        if(repository == null){
+            tv.Mensaje("No hay ningun vendedor por el momento");
+            return null;
+        }else
+            return repository.listarTodos();
+    }
+    public void borrarSeller(Seller s){
+        repository.eliminar(s.getId());
+    }
+    public void agregarSeller(Seller s){
+        repository.agregar(s);
 
     }
-    public publicarPropiedadEnVenta(String nombrePropiedad, int sellerId){
-
+    public Seller buscarSeller(int id){
+        return repository.buscar(id);
     }
+
     
 }
