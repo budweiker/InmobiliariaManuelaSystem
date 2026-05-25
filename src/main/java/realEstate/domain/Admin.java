fbfa9ffa6a8729e8c98c77a76219e05d9f8d7153
Andres Orozco <andresorozco@Andress-MacBook-Air.local>
2026-05-25 12:31:15 -0500

    SuperUser edit and diagram included

--- src/main/java/realEstate/domain/Admin.java
+++ src/main/java/realEstate/domain/Admin.java
@@ -1,8 +1,22 @@
 package realEstate.domain;
 
 public class Admin extends User {
+    private String adminType;
+
     public Admin(){
-        
+        super();
     }
-    
+
+    public Admin(int id, String nombre, float balance, String telefono, String correo, String adminType) {
+        super(id, nombre, balance, telefono, correo);
+        this.adminType = adminType;
+    }
+
+    public String getAdminType() {
+        return adminType;
+    }
+
+    public void setAdminType(String adminType) {
+        this.adminType = adminType;
+    }
 }
