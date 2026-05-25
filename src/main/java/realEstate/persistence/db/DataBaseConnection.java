e54ab1dc96ce8763285dced7e3ad49a93658c231
Andres Orozco <andresorozco@Andress-MacBook-Air.local>
2026-05-24 21:10:35 -0500

    Data base connection development

---
+++ src/main/java/realEstate/persistence/db/DataBaseConnection.java
@@ -0,0 +1,34 @@
+package realEstate.persistence.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseConnection {
+        private static DataBaseConnection instance;
+        private final Connection connection;
+
+        private static final String URL = "jdbc:mysql://localhost:3306/inmobiliaria_Manuela_Bermudez_Realtor";
+        private static final String USER = "root";
+        private static final String PASSWORD = "";
+
+        private DataBaseConnection() {
+            try{
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println("Conexión a la base de datos establecida");
+            } catch (SQLException e) {
+                throw new RuntimeException("Error al conectar a la base de datos",e);
+            }
+        }
+
+        public static synchronized DataBaseConnection getInstance() {
+            if (instance == null) {
+                instance = new DataBaseConnection();
+            }
+            return instance;
+        }
+
+        public Connection getConnection() {
+            return connection;
+        }
+}
